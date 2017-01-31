22e9a2ad97eb9ac70798709df49b34121b1a66ab
Sebastian Kippe <sebastian@kip.pe>
2017-01-31 11:56:51 +0800

    Basic layout

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        'bower_components/bourbon/app/assets/stylesheets',
+        'bower_components/neat/app/assets/stylesheets'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
