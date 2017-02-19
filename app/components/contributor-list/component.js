01c1a9c80404087dd9690193d6b1a9763f7b4694
Sebastian Kippe <sebastian@kip.pe>
2017-02-19 11:13:14 +0800

    Toggle metadata visibility by clicking name

--- app/components/contributor-list/component.js
+++ app/components/contributor-list/component.js
@@ -3,6 +3,20 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   tagName: 'table',
-  classNames: 'contributor-list'
+  classNames: 'contributor-list',
+  selectedContributor: null,
+
+  actions: {
+
+    toggleContributorInfo(contributor) {
+      if (contributor.get('showMetadata')) {
+        contributor.set('showMetadata', false);
+      } else {
+        this.get('contributors').setEach('showMetadata', false);
+        contributor.set('showMetadata', true);
+      }
+    }
+
+  }
 
 });
