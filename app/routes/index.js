b125d941ba99148223beeabc1f9b24187eb44c00
Garret Alfert <alfert@wevelop.de>
2017-06-14 01:55:39 +0200

    Ask user to unlock their account

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,20 @@ export default Ember.Route.extend({
 
   kredits: Ember.inject.service(),
 
+  beforeModel(transition) {
+    const kredits = this.get('kredits');
+
+    if (kredits.get('web3') && kredits.get('web3Provided')) {
+      kredits.get('web3').eth.getAccounts((error, accounts) => {
+        if (error || accounts.length === 0) {
+          if (confirm('Please unlock your accounts')) {
+            transition.retry();
+          }
+        }
+      });
+    }
+  },
+
   model() {
     let kredits = this.get('kredits');
 
