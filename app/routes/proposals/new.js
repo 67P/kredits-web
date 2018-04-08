31a1f21dcd3ff8d767886b48d1dfa16f7a68005c
Manuel Wiedenmann <manuel@funkensturm.de>
2018-04-08 02:13:07 +0200

    Fix linting

--- app/routes/proposals/new.js
+++ app/routes/proposals/new.js
@@ -4,12 +4,12 @@ import Route from 'ember-route';
 export default Route.extend({
   kredits: injectService(),
 
-  setupController(controller, model) {
+  setupController(controller) {
     this._super(...arguments);
 
     this.get('kredits').getContributors()
       .then((contributors) => {
-        this.controller.set('contributors', contributors);
+        controller.set('contributors', contributors);
       });
   }
 });
