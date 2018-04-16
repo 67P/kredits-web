8d303ea5af0455cab188a8289df9d0f000ff7e10
Sebastian Kippe <sebastian@kip.pe>
2018-04-17 01:38:21 +0200

    Fix tests, remove obsolete initializers

--- tests/unit/models/contributor-test.js
+++ tests/unit/models/contributor-test.js
@@ -1,13 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-
-import { run } from '@ember/runloop';
+import Contributor from 'kredits-web/models/contributor';
 
 module('Unit | Model | contributor', function(hooks) {
   setupTest(hooks);
 
   test('#avatarURL() returns correct URL', function(assert) {
-    let model = run(() => this.owner.lookup('service:store').createRecord('contributor'));
+    let model = Contributor.create();
     model.set('github_uid', '318');
 
     assert.equal(model.get('avatarURL'), 'https://avatars2.githubusercontent.com/u/318?v=3&s=128');
