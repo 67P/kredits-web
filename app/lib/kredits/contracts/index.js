0e556aa7947b1c888ccc7b2ce2d140bb5cc25b28
bumi <hello@michaelbumann.com>
2018-04-18 19:01:26 +0200

    Remove kredits library files and use npm package
    Currently it uses the package directly from git

--- app/lib/kredits/contracts/index.js
+++
@@ -1,9 +0,0 @@
-import Contributor from './contributor';
-import Operator from './operator';
-import Token from './token';
-
-export default {
-  Contributors: Contributor,
-  Operator,
-  Token
-};
