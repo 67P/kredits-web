889f2c05fcdc99cfe37f3677fc94c10cebf1c405
Michael Bumann <hello@michaelbumann.com>
2019-04-03 13:43:57 +0200

    Fix bignumber computed property
    The ethers.js bignumber API changed in v4.x

--- app/utils/cps/bignumber.js
+++ app/utils/cps/bignumber.js
@@ -5,16 +5,16 @@ export default function(dependentKey, converterMethod) {
   return computed(dependentKey, {
     get () {
       let value = this.get(dependentKey);
-      if (value && ethers.utils.isBigNumber(value)) {
+      if (value && ethers.utils.BigNumber.isBigNumber(value)) {
         return value[converterMethod]();
       } else {
         return value;
       }
     },
     set (key, value) {
-      value = ethers.utils.bigNumberify(value);
-      this.set(dependentKey, value);
-      return value[converterMethod]();
+      const bnValue = ethers.utils.bigNumberify(value);
+      this.set(dependentKey, bnValue);
+      return bnValue[converterMethod]();
     }
   });
 }
