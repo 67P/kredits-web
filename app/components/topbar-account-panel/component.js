c7d046aa464bff9d098444bdc4e981381407311f
Sebastian Kippe <sebastian@kip.pe>
2019-07-18 19:26:16 +0200

    Move topbar account info to component, add tests
    Adds a failing test for users with wallet, but no known contributor
    account, where it currently shows no name at all.

---
+++ app/components/topbar-account-panel/component.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+
+  tagName: '',
+
+  kredits: service(),
+
+});
---
+++ tests/integration/components/topbar-account-panel/component-test.js
@@ -0,0 +1,39 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | topbar-account-panel', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('unknown user without wallet (or no permission to get wallet/account info)', async function(assert) {
+    await render(hbs`<TopbarAccountPanel />`);
+
+    assert.equal(this.element.textContent.trim(), 'Anonymous');
+  });
+
+  test('unknown user with Ethereum wallet', async function(assert) {
+    let service = this.owner.lookup('service:kredits');
+    service.set('currentUserAccounts', [{ foo: 'bar' }]);
+    await render(hbs`<TopbarAccountPanel />`);
+
+    assert.equal(this.element.textContent.trim(), 'Anonymous');
+  });
+
+  test('known contributor', async function(assert) {
+    let service = this.owner.lookup('service:kredits');
+    service.set('currentUserAccounts', [{ foo: 'bar' }]);
+    service.set('currentUser', {
+      name: 'Dorian Nakamoto',
+      isCore: false
+    });
+    await render(hbs`<TopbarAccountPanel />`);
+
+    assert.equal(this.element.textContent.trim(), 'Dorian Nakamoto');
+
+    service.set('currentUser.isCore', true);
+    await render(hbs`<TopbarAccountPanel />`);
+
+    assert.equal(this.element.querySelectorAll('span.core-flag').length, 1);
+  });
+});
