89391c154334fbbda510295a0c8167752fdd08de
Sebastian Kippe <sebastian@kip.pe>
2019-09-10 15:21:50 +0200

    Update details of pending contribution when mined
    Fetches complete details of new contributions and replaces any potential
    pending, incomplete objects in the collection.

--- app/services/kredits.js
+++ app/services/kredits.js
@@ -225,7 +225,6 @@ export default Service.extend({
         console.debug('[kredits] add contribution response', data);
         attributes.contributor = this.contributors.findBy('id', attributes.contributorId);
         const contribution = Contribution.create(attributes);
-        // TODO receive from wrapper
         contribution.set('confirmedAtBlock', data.blockNumber + 40320);
         this.contributions.pushObject(contribution);
         return contribution;
@@ -315,6 +314,7 @@ export default Service.extend({
       .on('ContributorAdded', this.handleContributorChange.bind(this))
 
     this.kredits.Contribution
+      .on('ContributionAdded', this.handleContributionAdded.bind(this))
       .on('ContributionVetoed', this.handleContributionVetoed.bind(this))
 
     this.kredits.Proposal
@@ -340,6 +340,24 @@ export default Service.extend({
 
     console.debug('[kredits] new contributor', newContributor);
     this.contributors.pushObject(newContributor);
+  },
+
+  async handleContributionAdded (id, contributorId, amount) {
+    console.debug('[kredits] ContributionAdded event received', { id, contributorId, amount });
+
+    const pendingContribution = this.contributions.find(c => {
+      return (c.id === null) &&
+             (c.contributorId.toString() === contributorId.toString()) &&
+             (c.amount.toString() === amount.toString());
+    });
+
+    if (pendingContribution) {
+      const attributes = await this.kredits.Contribution.getById(id);
+      attributes.contributor = this.contributors.findBy('id', attributes.contributorId.toString());
+      const newContribution = Contribution.create(attributes);
+      this.contributions.addObject(newContribution);
+      this.contributions.removeObject(pendingContribution);
+    }
   },
 
   handleContributionVetoed (contributionId) {
