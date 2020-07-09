fb267813fb8805e151940db609eae6c9c7895ea8
Sebastian Kippe <sebastian@kip.pe>
2020-07-09 22:41:11 +0200

    Introduce item-list CSS, move item colors to variables

--- app/helpers/sats-to-btc.js
+++ app/helpers/sats-to-btc.js
@@ -1,5 +1,5 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function satsToBtc(amount/*, hash*/) {
-  return amount / 100000000;
+export default helper(function satsToBtc(params/*, hash*/) {
+  return params;
 });
