6c7de97e387c78b1fa702fe0c50672042fcaa29b
Râu Cao <raucao@kip.pe>
2023-01-11 14:10:35 +0800

    Get duration in human time

--- app/components/confirmed-in/component.js
+++ app/components/confirmed-in/component.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import moment from 'moment';
 
 export default class ConfirmedInComponent extends Component {
   @service kredits;
@@ -19,5 +20,11 @@ export default class ConfirmedInComponent extends Component {
   get confirmedInSeconds () {
     // A new block is mined every 30 seconds on average
     return this.confirmedInBlocks * 30;
+  }
+
+  get confirmedInHumanTime () {
+    console.debug(this.confirmedInBlocks);
+    console.debug(this.confirmedInSeconds);
+    return moment.duration(this.confirmedInSeconds, "seconds").humanize();
   }
 }
