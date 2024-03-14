3ee140723ca76482bdfe10c665d5236f4d079e1e
Râu Cao <raucao@kosmos.org>
2024-03-14 17:49:12 +0100

    Hide line items by default for confirmed reimbursements

--- app/components/reimbursement-item/component.js
+++ app/components/reimbursement-item/component.js
@@ -1,10 +1,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import config from 'kredits-web/config/environment';
 
 export default class ReimbursementItemComponent extends Component {
   @service kredits;
+  @tracked showExpenseDetails = false;
+
+  constructor(owner, args) {
+    super(owner, args);
+    if (this.isUnconfirmed && !this.args.reimbursement.vetoed) {
+      this.showExpenseDetails = true;
+    }
+  }
 
   get ipfsGatewayUrl () {
     return config.ipfs.gatewayUrl;
@@ -20,6 +29,11 @@ export default class ReimbursementItemComponent extends Component {
 
   get showVetoButton () {
     return this.isUnconfirmed && this.kredits.currentUserIsCore;
+  }
+
+  @action
+  toggleExpenseDetails () {
+    this.showExpenseDetails = !this.showExpenseDetails;
   }
 
   @action
